40cf90abcccd93751acbef788ef1e4f3b3a35129
jatziri2 <jatzirilin@ciencias.unam.mx>
2022-10-22 12:30:51 -0500

    Prototipo de producto (no es seguro)

---
+++ HornoM.java
@@ -0,0 +1,9 @@
+public class HornoM extends producto{
+    
+    public HornoM(){
+        codigo_de_barras = 123453457;
+        nombre = "Horno";
+        departamento = "Electrodomesticos";
+        precio = 4000;
+    }
+}
---
+++ Microondas.java
@@ -0,0 +1,9 @@
+public class Microondas extends producto {
+    
+    public Microondas(){
+        codigo_de_barras = 12345667;
+        nombre = "Microondas";
+        departamento = "Electrodomesticos";
+        precio = 3500;
+    }
+}
