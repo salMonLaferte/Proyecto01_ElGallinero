e286455495e5afcdae9607aca8afc6ee20107b4c
jatziri2 <jatzirilin@ciencias.unam.mx>
2022-10-22 20:30:03 -0500

    Se agrego lo del catalogo 22-10-22

--- src/Catalogo.java
+++ src/Catalogo.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class Catalogo {
-    private ArrayList<Producto> productos;
+
+    protected ArrayList<Producto> productos;
 
     public Catalogo(boolean crearloVacio){
         productos = new ArrayList<Producto>();
@@ -18,5 +22,34 @@ public class Catalogo {
     }
     public Iterator<Producto> obtenerIterador(){
         return productos.iterator();
+    }
+     
+    protected void obtenerTextos(){
+        File archivo = null;
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+        try{
+            archivo = new File("catalogo.txt");
+            fileReader = new FileReader(archivo);
+            bufferedReader = new BufferedReader(fileReader);
+            String linea;
+            while( (linea = bufferedReader.readLine() ) != null){
+                String[] keyVal = linea.split("--");
+                if(keyVal.length == 4){
+                    Producto producto = new Producto(Long.parseLong(keyVal[0]), keyVal[1], keyVal[2], Double.parseDouble(keyVal[3]));
+                    productos.add(producto);
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(null != fileReader){
+                    fileReader.close();
+                }
+            }catch(Exception e2){
+                e2.printStackTrace();
+            }
+        }
     }
 }
--- src/Producto.java
+++ src/Producto.java
@@ -1,10 +1,16 @@
-public abstract class Producto{
+public class Producto{
     /**Codigo de barras que caracteriza cada producto */
     protected long codigo_de_barras;
     protected String nombre;
     protected String departamento;
     protected double precio;
 
+    public Producto(long codigo_de_barras, String nombre, String departamento, double precio){
+        this.codigo_de_barras = codigo_de_barras;
+        this.nombre = nombre;
+        this.departamento = departamento;
+        this.precio = precio;
+    }
     @Override
     public final String toString(){
         String producto = "Codigo de barras: " + codigo_de_barras + "\n";
